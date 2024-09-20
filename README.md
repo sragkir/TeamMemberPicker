28f140cfe1d642b9f941e232d797f3e6ee42b638
SragKir <sragkir@gmail.com>
2024-09-20 00:34:25 -0400

    AD User filter with User, AADUser, TeamMembership dataverse table

--- README.md
+++ README.md
@@ -1,1 +1,130 @@
-# TeamMemberPicker
+# PeoplePickerV1
+A PCF component for _"**Multi**" or "**single**"_ select people picker.
+
+This solution gives the ability to select Single User or Multiple users from the people picker. It works on the _**User(systemuser)**_ table or _**AAD User(aaduser)**_ table or _**team(teammembership)**_ table of the dataverse.
+
+Since multi-select peoplepicker is not there yet, this can be used as an alternate. It is build on top of Multiline text field. Moreover we have some filtering issue while using lookup column with AAD user table to overcome filtering problem, we will use this control as solution.
+
+# Control Features:
+  •	_We can use this control as "Single user select"._
+  
+  •	_We can use this control as "Multi user select"._
+  
+  •	_This control will access users from "AAD User" Table or "User" table or teammembership table to get specific AD group users._
+  
+  
+# Dependencies:
+office-ui-fabric-react : https://github.com/microsoft/fluentui
+
+
+# Reference:
+
+Build using this example :https://developer.microsoft.com/en-us/fluentui#/controls/web/peoplepicker 
+
+and 
+
+https://github.com/HanumantPatil/PeoplePickerV1
+
+
+# Usage:
+
+# Step 1:
+
+
+# Option 1:
+
+Import the zip file directly into CDS. Managed or Unmanaged Solution.
+
+# Option 2:
+
+git clone the repo
+
+npm install
+
+npm run build
+
+# Step 2 - Add the component
+
+Create a Multiline text field and add the component.
+
+#Step 3:
+
+1. Three parameters need to configure as shown below 
+
+<img width="445" alt="image" src="https://user-images.githubusercontent.com/18139035/178974887-9241d8e6-d156-43c4-97aa-d99bfe19a05d.png">
+
+entityName: aaduser or user
+
+<img width="416" alt="image" src="https://user-images.githubusercontent.com/18139035/178975166-82910ff8-f7ba-404a-90be-1c6c7106c1a4.png">
+
+selectionType: Single or Multi
+
+<img width="416" alt="image" src="https://user-images.githubusercontent.com/18139035/178975292-959a936b-0866-4c9b-b3db-f5d4a766e4d9.png">
+
+loadEnabledAccounts: true or false (applicable to only AAD User table)
+
+Load AAD User Enabled/Active Accounts or all accounts - true/false 
+
+<img width="409" alt="image" src="https://user-images.githubusercontent.com/18139035/178975605-1855b2f3-94dd-4d19-9151-aa455265da16.png">
+
+
+# sample
+
+<img width="680" alt="image" src="https://user-images.githubusercontent.com/18139035/178976649-759e48bf-95d0-4028-b746-f26bbe5e7513.png">
+
+
+<img width="442" alt="image" src="https://user-images.githubusercontent.com/18139035/178976736-73ccef1d-efad-4079-b3b1-1da93c8d05bb.png">
+
+# For packaging & deploying the solution, refer the below link
+
+https://docs.microsoft.com/en-us/power-apps/developer/component-framework/import-custom-controls
+
+# To use PCF control in Business Process Flow, follow the links below:
+
+_**https://powerusers.microsoft.com/t5/Power-Automate-Community-Blog/Preview-Custom-Controls-in-Business-Process-Flows/ba-p/263237**_
+
+_**https://debajmecrm.com/custom-controls-in-dynamics-365-business-process-flows/**_
+
+<img width="310" alt="image" src="https://user-images.githubusercontent.com/18139035/182327854-1fbb563d-5a8a-4723-93a7-c250261e315a.png">
+
+
+# if you faced problem to load PCF control first time then try below options:
+
+Option I:
+1. Remove 'async' from init or updateview method.
+
+Option II:
+
+1.	Change the control visibility of the control to No.
+
+<img width="616" alt="image" src="https://user-images.githubusercontent.com/18139035/182326702-bc54d6d6-9ddc-42e2-9e2d-8d6d876c0833.png">
+
+2.	Onload event of Main form, you can make that control visible Yes using script 
+
+<img width="440" alt="image" src="https://user-images.githubusercontent.com/18139035/182326910-8a04666e-d7c6-4548-984c-ea062b5ad72e.png">
+
+/**
+ *  Script to overcome PCF control issue not loading on the first load
+ *  Unhide the People Picker control.
+ **/
+ 
+ 
+ 
+VisiblePeoplePickerColumns = function (executionContext) {
+  var formContext = executionContext.getFormContext();
+  formContext.getControl("controlLogicalName").setVisible(true);
+  }
+
+
+
+# Solution:
+
+PeoplePickerV1Solution(PeoplePickerV1)
+
+# Author:
+
+Hanumant Patil
+
+# Disclaimer:
+
+_**THIS CODE IS PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**_
---
+++ TeamMemberPicker/TeamMemberPicker.tsx
@@ -0,0 +1,297 @@
+import * as React from 'react';
+import { IPersonaProps } from 'office-ui-fabric-react/lib/Persona';
+import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+import { IInputs } from "./generated/ManifestTypes";
+
+import {
+  IBasePickerSuggestionsProps,
+  IBasePicker,
+  NormalPeoplePicker,
+  ValidationState
+} from 'office-ui-fabric-react/lib/Pickers';
+
+export interface IPeoplePersona {
+  text?: string;
+  secondaryText?: string;
+}
+
+export interface ITeamMemberPickerProps {
+  people?: any;
+  preselectedpeople?: any;
+  context?: ComponentFramework.Context<IInputs>;
+  peopleList?: (newValue: any) => void;
+  isPickerDisabled?: boolean;
+}
+
+export interface ITeamMemberPickerState {
+  currentPicker?: number | string;
+  delayResults?: boolean;
+  peopleList: IPersonaProps[];
+  mostRecentlyUsed: IPersonaProps[];
+  currentSelectedItems?: IPersonaProps[];
+}
+
+const suggestionProps: IBasePickerSuggestionsProps = {
+  // suggestionsHeaderText: 'Suggested People',
+  // mostRecentlyUsedHeaderText: 'Suggested Contacts',
+  // noResultsFoundText: 'No results found',
+  loadingText: 'Loading',
+  showRemoveButtons: false,
+  // suggestionsAvailableAlertText: 'People Picker Suggestions available',
+  // suggestionsContainerAriaLabel: 'Suggested contacts'
+};
+
+
+export class TeamMemberPickerTypes extends React.Component<any, ITeamMemberPickerState> {
+  // All pickers extend from BasePicker specifying the item type.
+  private _picker = React.createRef<IBasePicker<IPersonaProps>>();
+
+  constructor(props: ITeamMemberPickerProps) {
+    super(props);
+
+    this.state = {
+      currentPicker: 1,
+      delayResults: false,
+      peopleList: this.props.people,
+      mostRecentlyUsed: [],
+      currentSelectedItems: []
+    };
+    initializeIcons();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  public render() {
+    return (
+      this.props.context.parameters.selectionType.raw! === "Single" ?
+        <NormalPeoplePicker
+          onResolveSuggestions={this._onFilterChanged}
+          onEmptyInputFocus={this._returnMostRecentlyUsed}
+          getTextFromItem={this._getTextFromItem}
+          pickerSuggestionsProps={suggestionProps}
+          className={'ms-PeoplePicker'}
+          key={'normal'}
+          onRemoveSuggestion={this._onRemoveSuggestion}
+          onValidateInput={this._validateInput}
+          removeButtonAriaLabel={'Remove'}
+          defaultSelectedItems={this.props.preselectedpeople}
+          onItemSelected={this._onItemSelected}
+          itemLimit={1}
+          inputProps={{
+            onBlur: (ev: React.FocusEvent<HTMLInputElement>) => {
+            },
+            onFocus: (ev: React.FocusEvent<HTMLInputElement>) => {
+            },
+            'aria-label': 'People Picker'
+          }}
+          componentRef={this._picker}
+          onInputChange={this._onInputChange}
+          resolveDelay={300}
+          disabled={this.props.isPickerDisabled}
+          onChange={() => this.handleChange()}
+        />
+        :
+        <NormalPeoplePicker
+          onResolveSuggestions={this._onFilterChanged}
+          onEmptyInputFocus={this._returnMostRecentlyUsed}
+          getTextFromItem={this._getTextFromItem}
+          pickerSuggestionsProps={suggestionProps}
+          className={'ms-PeoplePicker'}
+          key={'normal'}
+          onRemoveSuggestion={this._onRemoveSuggestion}
+          onValidateInput={this._validateInput}
+          removeButtonAriaLabel={'Remove'}
+          defaultSelectedItems={this.props.preselectedpeople}
+          onItemSelected={this._onItemSelected}
+          inputProps={{
+            onBlur: (ev: React.FocusEvent<HTMLInputElement>) => {
+            },
+            onFocus: (ev: React.FocusEvent<HTMLInputElement>) => {
+            },
+            'aria-label': 'People Picker'
+          }}
+          componentRef={this._picker}
+          onInputChange={this._onInputChange}
+          resolveDelay={300}
+          disabled={this.props.isPickerDisabled}
+          onChange={() => this.handleChange()}
+        />
+    );
+
+  }
+
+  private handleChange() {
+    if (this._picker !== undefined) {
+      this.getPeopleRelatedVal();
+    }
+  }
+
+  private _onItemSelected = (item: any): Promise<IPersonaProps> => {
+    const processedItem = { ...item };
+    processedItem.text = `${item.text}`;
+    return new Promise<IPersonaProps>((resolve, reject) =>
+      resolve(processedItem));
+  };
+
+
+
+  private async getPeopleRelatedVal() {
+    try {
+      const tempPeople: IPeoplePersona[] = this._picker.current!.items!.map(item => ({
+        text: item.text,
+        secondaryText: item.secondaryText
+      }));
+      if (this.props.peopleList) {
+        this.props.peopleList(tempPeople);
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  private _getTextFromItem(persona: IPersonaProps): string {
+    return persona.text as string;
+  }
+
+  private _onRemoveSuggestion = (item: IPersonaProps): void => {
+    const { peopleList, mostRecentlyUsed: mruState } = this.state;
+    const indexPeopleList: number = peopleList.indexOf(item);
+    const indexMostRecentlyUsed: number = mruState.indexOf(item);
+
+    if (indexPeopleList >= 0) {
+      const newPeople: IPersonaProps[] = peopleList.slice(0, indexPeopleList).concat(peopleList.slice(indexPeopleList + 1));
+      this.setState({ peopleList: newPeople });
+    }
+
+    if (indexMostRecentlyUsed >= 0) {
+      const newSuggestedPeople: IPersonaProps[] = mruState
+        .slice(0, indexMostRecentlyUsed)
+        .concat(mruState.slice(indexMostRecentlyUsed + 1));
+      this.setState({ mostRecentlyUsed: newSuggestedPeople });
+    }
+  }
+
+  private _onFilterChanged = (
+    filterText: string,
+    currentPersonas: IPersonaProps[] | undefined,
+    limitResults?: number
+  ): any => {
+    if (filterText) {
+      if (filterText.length > 2) {
+        return this._searchUsers(filterText);
+      }
+    } else {
+      return [];
+    }
+  };
+
+  // private async _searchUsers(filterText: string): Promise<IPersonaProps[]> {
+  private _searchUsers(filterText: string): IPersonaProps[] | Promise<IPersonaProps[]> {
+    return new Promise(async (resolve: any, reject: any) => {
+      let People: IPersonaProps[] = [];
+
+      try {
+        const entityName = this.props.context.parameters.entityName.raw!;
+        let tempPeople: any;
+
+        if (entityName === "systemuser") {
+          tempPeople = await this.props.context.webAPI.retrieveMultipleRecords(entityName, `?$select=fullname,internalemailaddress&$filter=startswith(fullname,'${filterText}')`);
+          People = tempPeople.entities.map((entity: any) => ({
+            text: entity.fullname,
+            secondaryText: entity.internalemailaddress
+          }));
+        } else if (entityName === "aaduser") {
+          tempPeople = await this.props.context.webAPI.retrieveMultipleRecords(entityName, `?$select=displayname,mail,accountenabled&$filter=startswith(displayname,'${filterText}') and usertype eq 'member' and mail ne null`);
+          People = tempPeople.entities
+            .filter((entity: any) =>
+              this.props.context.parameters.loadEnabledAccounts.raw! === 'true'
+                ? entity.accountenabled
+                : true
+            )
+            .map((entity: any) => ({
+              text: entity.displayname,
+              secondaryText: entity.mail
+            }));
+        } else if (entityName === "team" && this.props.context.parameters.teamId.raw! !== "12345678-1234-1234-1234-123456789012") {
+          tempPeople = await this.props.context.webAPI.retrieveMultipleRecords("teammemberships", `?$select=systemuserid&$filter=teamid eq ${this.props.context.parameters.teamId}`);
+          const usersMatched = await this.props.context.webAPI.retrieveMultipleRecords(entityName, `?$select=fullname,internalemailaddress,systemuserid&$filter=startswith(fullname,'${filterText}')`);
+          const matchedTeamUsers = usersMatched.entities.filter((user: any) => tempPeople.entities.some((people: any) => people.systemuserid === user.systemuserid));
+          People = matchedTeamUsers.map((entity: any) => ({
+            text: entity.fullname,
+            secondaryText: entity.internalemailaddress
+          }));
+        }
+
+        resolve(People);
+      } catch (err) {
+        console.log(err);
+        reject(People);
+      }
+    });
+  }
+
+  private _returnMostRecentlyUsed = (currentPersonas: any): IPersonaProps[] | Promise<IPersonaProps[]> => {
+    let { mostRecentlyUsed } = this.state;
+    mostRecentlyUsed = this._removeDuplicates(mostRecentlyUsed, currentPersonas);
+    return this._filterPromise(mostRecentlyUsed);
+  };
+
+  private _filterPromise(personasToReturn: IPersonaProps[]): IPersonaProps[] | Promise<IPersonaProps[]> {
+    if (this.state.delayResults) {
+      return this._convertResultsToPromise(personasToReturn);
+    } else {
+      return personasToReturn;
+    }
+  }
+
+  private _listContainsPersona(persona: IPersonaProps, personas: IPersonaProps[]) {
+    if (!personas || !personas.length || personas.length === 0) {
+      return false;
+    }
+    return personas.filter(item => item.text === persona.text).length > 0;
+  }
+
+  // private _filterPersonasByText(filterText: string): IPersonaProps[] {
+  //     return this.state.peopleList.filter(item => this._doesTextStartWith(item.text as string, filterText));
+  // }
+
+  // private _doesTextStartWith(text: string, filterText: string): boolean {
+  //     return text.toLowerCase().indexOf(filterText.toLowerCase()) === 0;
+  // }
+
+  private _convertResultsToPromise(results: IPersonaProps[]): Promise<IPersonaProps[]> {
+    return new Promise<IPersonaProps[]>((resolve, reject) => setTimeout(() => resolve(results), 2000));
+  }
+
+  private _removeDuplicates(personas: IPersonaProps[], possibleDupes: IPersonaProps[]) {
+    return personas.filter(persona => !this._listContainsPersona(persona, possibleDupes));
+  }
+
+  private _validateInput = (input: string): ValidationState => {
+    if (input.indexOf('@') !== -1) {
+      return ValidationState.valid;
+    } else if (input.length > 1) {
+      return ValidationState.warning;
+    } else {
+      return ValidationState.invalid;
+    }
+  }
+
+  /**
+   * Takes in the picker input and modifies it in whichever way
+   * the caller wants, i.e. parsing entries copied from Outlook (sample
+   * input: "Aaron Reid <aaron>").
+   *
+   * @param input The text entered into the picker.
+   */
+  private _onInputChange(input: string): string {
+    const outlookRegEx = /<.*>/g;
+    const emailAddress = outlookRegEx.exec(input);
+
+    if (emailAddress && emailAddress[0]) {
+      return emailAddress[0].substring(1, emailAddress[0].length - 1);
+    }
+    return input;
+  }
+
+}
